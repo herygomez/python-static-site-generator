28fd6159bf19663f9aba5ca39dc32b83c2d463e7
herygomez <godelfer25@outlook.com>
2022-09-26 09:21:13 -0600

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -5,7 +5,7 @@ class Site:
 
     def __init__(self, source, dest):
         self.source = Path(source)
-        self.dest = Path(source)
+        self.dest = Path(dest)
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
